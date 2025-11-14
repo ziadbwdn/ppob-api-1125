7158a5c61c80cc3ab24769a9860eb536a9c15433
ziadbwdn <user@LAPTOP-SDH7GN63.localdomain>
2025-11-14 18:53:35 +0700

    reset versioning on server files

--- config/app.config.js
+++
@@ -1,9 +0,0 @@
-// config/app.config.js
-const env = require('./env');
-
-module.exports = {
-  nodeEnv: env.NODE_ENV,
-  port: env.PORT,
-  apiPrefix: env.API_PREFIX,
-  corsOrigin: env.CORS_ORIGIN,
-};
--- config/jwt.config.js
+++
@@ -1,7 +0,0 @@
-// config/jwt.config.js
-const env = require('./env');
-
-module.exports = {
-  secret: env.JWT_SECRET,
-  expiresIn: env.JWT_EXPIRATION,
-};
